7d89e7d539305b3ce18b00ef586b043c9322b315
Sean <syoo9053@gmail.com>
2019-06-20 16:30:23 -0400

    Modify File Upload to Fix Async Problem

--- src/App.js
+++ src/App.js
@@ -7,32 +7,14 @@ class App extends Component {
   constructor(props) {
 		super(props);
 		this.state = {
-			input: 'NA',
-			byteArray: []
 		}
   }
 
-  onInputChange1 = (e) => {
-    var files = e.target.files;
-    this.setState({input: files}, function () {});
-	}
-
-	onButtonSubmit1 = () => {
-    if (this.state.input !== 'NA') {
-      var r = new FileReader();
-      r.onload = function(){ alert(r.result); };
-      r.readAsBinaryString(this.state.input[0]);
-      console.log('hello');
-    }
-	}
   
   render () {
     return (
       <div className="App">
-        <Compress
-          onInputChange1={this.onInputChange1}
-          onButtonSubmit1={this.onButtonSubmit1}
-        />
+        <Compress/>
         {/* <Decompress/> */}
       </div>
     );
--- src/components/Compress/Compress.js
+++ src/components/Compress/Compress.js
@@ -1,13 +1,68 @@
 import React from 'react';
 import './Compress.css';
 
-const Compress = ({ onInputChange1, onButtonSubmit1 }) => {
+const Compress = () => {
+	var input = '';
+	var fileByteArray = [];
+	var contentBuffer = undefined;
+
+	function onInputChange(e) {
+		input = e.target;
+		console.log("ready");
+	}
+
+	function onButtonClick() {
+		if (input !== '') {
+			processFile()
+		}
+
+		
+
+	}
+
+	function readFileAsync(file) {
+		return new Promise((resolve, reject) => {
+		  let reader = new FileReader();
+	    
+		  reader.onload = () => {
+		    resolve(reader.result);
+		  };
+	    
+		  reader.onerror = reject;
+	    
+		  reader.readAsArrayBuffer(file);
+		})
+	    }
+	    
+	async function processFile() {
+		try {
+			let file = input.files[0];
+			contentBuffer = await readFileAsync(file);
+			var array = new Uint8Array(contentBuffer);
+			fileByteArray= [];
+				for (var i = 0; i < array.length; i++) {
+				    fileByteArray.push(array[i]);
+				}
+
+			huffmancoding(fileByteArray);
+
+		} catch(err) {
+			console.log(err);
+		}
+	}
+
+	function huffmancoding(fileByteArray) {
+		
+	}
+
+
 	return (
 		<div>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input className= 'f4 pa2 w-70 center' name= 'file' type="file" onChange={(e)=>onInputChange1(e)}/>
-					<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit1}>Compress</button>
+					<input className= 'f4 pa2 w-70 center' name= 'file' type="file" onChange={(e) => onInputChange(e)}/>
+					<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick= {onButtonClick}>Compress</button>
+					<span id="file"></span>
 				</div>
 			</div>
 		</div>
